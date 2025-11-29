c668b708ac886e38b7cb9f35ce2f064cba72dcd9
andres giraldo <andres18281@hotmail.com>
2025-11-29 14:55:43 -0500

    Commit inicial del proyecto

---
+++ src/main/java/com/example/demo/EntidadFinancieraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntidadFinancieraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntidadFinancieraApplication.class, args);
+	}
+
+}
